08eab90479ad4ba67eeca5b4293b1e3c5ff8ee02
michaeleliyahu <mmyy605@gmail.com>
2019-11-26 06:36:27 -0500

    first commit

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "myBank.h"
 #include <stdio.h>
-char chcekLimit(int account,char ans)
-{ 
-    if(account>950 || account<901)
+char chcekLimit(int account, char ans)
+{
+    if (account > 950 || account < 901)
     {
-        printf("Your number account is illegal! The number must be 901-950");
-        ans='E';
+        printf("Your number account is illegal! The number must be 901-950\n");
+        ans = 'E';
         return ans;
     }
     return ans;
@@ -21,26 +21,26 @@ int main()
     int num = 0;
     do
     {
-        printf("please enter ‫‪Transaction‬‬ ‫‪type‬‬ code \n 'O' for open account \n 'B' for balance inquiry ");
+        printf("\nplease enter ‫‪Transaction‬‬ ‫‪type‬‬ code: \n 'O' for open account \n 'B' for balance inquiry ");
         printf("\n 'D' for deposit funds \n 'W' for cash withrawal \n 'C' for close account");
         printf("\n 'I' for add interest \n 'P' for balance inquiry \n 'E' for close all accounts\n");
         scanf(" %c", &ans);
-        int count = 0;
-        while(getchar() != '\n')
-        {
-            count++;
-        }
-        if(count>0)
-        {
-            ans = 'v';
 
-        }
         switch (ans)
         {
-
+            // if(ans!='O'&&ans!='B'&&ans!='D'&&ans!='W'&&ans!='C'&&ans!='I'&&ans!='P'&&ans!='E')
+            //{
+            //  printf("This char ")
+            //}
         case 'O':
+            money = 0;
             printf("\nenter a deposit\n");
             scanf("%lf", &money);
+            if (money < 0)
+            {
+                printf("You must enter positive number!\n");
+                break;
+            }
             openAccount(money);
 
             break;
@@ -48,8 +48,8 @@ int main()
         case 'B':
             printf("Enter number of account:\n");
             scanf("%d", &account);
-            ans=chcekLimit(account,ans);
-            if (ans=='E')
+            ans = chcekLimit(account, ans);
+            if (ans == 'E')
             {
                 break;
             }
@@ -60,34 +60,46 @@ int main()
         case 'D':
             printf("Enter number of account:\n");
             scanf("%d", &account);
-             ans=chcekLimit(account,ans);
-            if (ans=='E')
+            ans = chcekLimit(account, ans);
+            if (ans == 'E')
             {
                 break;
             }
+            money = 0;
             printf("How much money do you want to diposit in account: \n");
             scanf("%lf", &money);
+            if (money < 0)
+            {
+                printf("You must enter positive number!\n");
+                break;
+            }
             deposit(account, money);
             break;
 
         case 'W':
             printf("Enter number of account:");
             scanf("%d", &account);
-             ans=chcekLimit(account,ans);
-            if (ans=='E')
+            ans = chcekLimit(account, ans);
+            if (ans == 'E')
             {
                 break;
             }
+            money = 0;
             printf("How much money do you want to attract from account: \n");
             scanf("%lf", &money);
+            if (money < 0)
+            {
+                printf("You must enter positive number!\n");
+                break;
+            }
             withrawal(account, money);
             break;
 
         case 'C':
-        printf("Enter number of account:");
+            printf("Enter number of account:");
             scanf("%d", &account);
-             ans=chcekLimit(account,ans);
-            if (ans=='E')
+            ans = chcekLimit(account, ans);
+            if (ans == 'E')
             {
                 break;
             }
@@ -95,8 +107,13 @@ int main()
             break;
 
         case 'I':
-         printf("Enter percent of interest:");
+            printf("Enter percent of interest:");
             scanf("%lf", &inter);
+                if (inter < 0)
+            {
+                printf("You must enter positive number!\n");
+                break;
+            }
             interest(inter);
             break;
 
@@ -110,8 +127,8 @@ int main()
             break;
 
         default:
+            printf("This char doesent correct \n");
             break;
         }
     } while (num != 1);
 }
-
